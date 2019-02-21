ba082f3652e95829eb0234dbe36e716451c6cf5a
Matt Ruston <mruston@underarmour.com>
2019-02-21 09:51:17 -0600

    Added swift file

---
+++ CarthageTestC/CarthageC.swift
@@ -0,0 +1,17 @@
+//
+//  CarthageC.swift
+//  CarthageTestC
+//
+//  Created by Matthew Ruston on 2/21/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+import Foundation
+
+public class CarthageC {
+    public static let shared = CarthageC()
+    
+    public func work() {
+        print("Carthage C Ran!")
+    }
+}
